a85fa1cd9790e32b356f2fd204376cc31024b81f
Thuvaraka <thuvathaya1919@gmail.com>
2021-08-15 02:09:53 +0530

    first commit

---
+++ app/src/main/java/com/example/hrb/MenuActivity.java
@@ -0,0 +1,35 @@
+package com.example.hrb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+    public void onClickBooking(View view) {
+        Intent intent = new Intent(this, BookingActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickPayment(View view) {
+        Intent intent = new Intent(this, PaymentActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickBookingDetails(View view) {
+        Intent intent = new Intent(this, BookingDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickBookingDetailsAdmin(View view) {
+        Intent intent = new Intent(this, BookingDetailsAdminActivity.class);
+        startActivity(intent);
+    }
+}
